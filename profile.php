1d2d722e4897d210495e7c0856879014e0f08447
Endi Merkuri <35932875+endimerkuri@users.noreply.github.com>
2019-05-12 20:57:44 +0300

    Added login, signup and profile source files.

---
+++ profile.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+
+	<body>
+		<nav class="navbar navbar-expand-sm bg-dark navbar-light" style="border-bottom: 2px solid red">
+		  <a class="navbar-brand mr-auto" href="#">
+			<img src="icon.jpeg" alt="Logo" style="height:50px">
+		  </a>
+
+		  <ul class="navbar-nav">
+		  <li class="nav-item">
+			  <a class="nav-link text-danger" href="#">Explore <span class= "glyphicon glyphicon-search"> </span></a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link text-danger" href="#">Messages <span class= "glyphicon glyphicon-envelope"> </span></a>
+			</li>
+			<li class="nav-item active">
+			  <a class="nav-link text-danger" href="#">Notifications <span class= "glyphicon glyphicon-bell"> </span> </a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link text-danger" href="#">Profile <span class= "glyphicon glyphicon-user"> </span> </a>
+			</li>
+		  </ul>
+		</nav>
+	</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	<!------ Include the above in your HEAD tag ---------->
+	</head>
+
+	<body>
+		<div class = "wrapper">
+		  <div id="formContent">
+			<!-- Tabs Titles -->
+
+			<!-- Icon -->
+			<div>
+			  <img class="img-fluid" src="icon.jpeg" id="icon" alt="GatherTogether"/>
+			</div>
+
+			<!-- Login Form -->
+			<form>
+			  <input type="text" id="login" class="fadeIn first" name="login" placeholder="Please enter username">
+			  <input type="text" id="password" class="fadeIn second" name="login" placeholder="Please enter password">
+			  <input type="text" id="password" class="fadeIn third" name="login" placeholder="Confirm password">
+			  <input type="submit" class="fadeIn fourth" value="Sign Up">
+			 </form>
+			   <!-- Remind Passowrd -->
+			<div id="formFooter">
+			  <p> Already have an account? <a class="underlineHover" href="login.php">Log In.</a> </p>
+			</div>
+
+		  </div>
+		</div>
+	</body>
+</html>
