ed9bbc6f57986d18302967034232bfad19f95799
ahmettalhasen <36816880+ahmettalhasen@users.noreply.github.com>
2019-05-19 21:55:20 +0300

    Database is connected.

--- signup.php
+++ signup.php
@@ -1,3 +1,48 @@
+<?php 
+    session_start();
+    $_SESSION['message'] = "";
+    $dbServer = "localhost";
+    $dbUsername = "talha.sen";
+    $dbPassword = "p2yjILda";
+    $dbName = "talha_sen";
+
+    $con = new mysqli($dbServer, $dbUsername, $dbPassword, $dbName);
+
+    if($con -> connect_error) {
+            die("Connection Failed: " . $con -> connect_error) . "<br>";
+    }
+
+    if(isset($_POST['registerButton'])) {
+        //echo "Button is Clicked. <br>" ;
+        
+        if($_POST['passwordReg'] == $_POST['passwordReg2']){
+            
+            //echo "Passwords are matching. <br>";
+            
+            $userReg = $con->real_escape_string($_POST["usernameReg"]);
+            $passReg = $con->real_escape_string($_POST["passwordReg"]);
+            $nameOfUser = $con->real_escape_string($_POST["nameOfUser"]);
+            $surnameOfUser = $con->real_escape_string($_POST["surnameOfUser"]);
+            $emailaddress = $con->real_escape_string($_POST["emailaddress"]);
+            
+            $nameTogether = ucwords($nameOfUser . " " . $surnameOfUser);
+            //echo "username: " . $userReg . "<br>";
+            //echo "password: " . $passReg;
+            $registerQuery = "INSERT INTO user(username, name, email, password) VALUES('$userReg', '$nameTogether', '$emailaddress', '$passReg');";
+            
+            if($con->query($registerQuery) === true) {
+                $_SESSION['user'] = $userReg;
+                header("location: profile.php");
+            } else {
+                $_SESSION['message'] = "<span style=\"color:red\"> Register is unsuccessful!</span>";
+            }
+          
+        } else{
+            $_SESSION['message'] = "<span style=\"color:red\"> Passwords do NOT match!</span>";
+        }
+    }               
+?>
+
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -10,29 +55,28 @@
 	<!------ Include the above in your HEAD tag ---------->
 	</head>
 
-	<body>
+	<body>      
 		<div class = "wrapper">
 		  <div id="formContent">
-			<!-- Tabs Titles -->
-
-			<!-- Icon -->
 			<div>
 			  <img class="img-fluid" src="icon.jpeg" id="icon" alt="GatherTogether"/>
 			</div>
-
-			<!-- Login Form -->
-			<form>
-			  <input type="text" id="login" class="fadeIn first" name="login" placeholder="Please enter username">
-			  <input type="text" id="password" class="fadeIn second" name="login" placeholder="Please enter password">
-			  <input type="text" id="password" class="fadeIn third" name="login" placeholder="Confirm password">
-			  <input type="submit" class="fadeIn fourth" value="Sign Up">
-			 </form>
-			   <!-- Remind Passowrd -->
+			<form class="form" method="post" action="signup.php" >
+              <div class="alert alert-error"><?= $_SESSION['message']?></div>
+              <input type="text" class="fadeIn first" name="nameOfUser" placeholder="Enter your name" required>
+              <input type="text" class="fadeIn second" name="surnameOfUser" placeholder="Enter your surname" required>
+			  <input type="text" class="fadeIn third" name="usernameReg" placeholder="Enter a username" required>
+              <input type="text" class="fadeIn fourth" name="emailaddress" placeholder="Enter your e-mail" required>
+			  <input type="password" class="fadeIn fifth" name="passwordReg" placeholder="Enter a password" required>
+			  <input type="password" class="fadeIn sixth" name="passwordReg2" placeholder="Confirm the password" required>
+              
+                
+			  <input type="submit" name = "registerButton" class="fadeIn seventh" value="Sign Up">
+			 </form> 
 			<div id="formFooter">
 			  <p> Already have an account? <a class="underlineHover" href="login.php">Log In.</a> </p>
 			</div>
-
 		  </div>
-		</div>
+		</div>   
 	</body>
 </html>
